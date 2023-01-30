5dc257a88192d1fc79a24d22f65a44bbc5ed51f9
Fazle Bari <fazlebarisn@hotmail.com>
2023-01-30 22:25:04 +0600

    add Auction Product tab

--- functions.php
+++ functions.php
@@ -3,12 +3,12 @@
 /**
  * This function will only register fbs_auction type product
  * all other function is inside include/Admin/RegisterProductType class
- * I will arrange this code leader
+ * I will arrange this code later
  *
  * @return void
  */
 function fbs_register_auction_product_type() {
-  class WC_Product_Demo extends WC_Product {
+  class Fbs_Auction_Product extends WC_Product {
             
     public function __construct( $product ) {
         $this->product_type = 'fbs_auction';
--- includes/Admin/RegisterProductType.php
+++ includes/Admin/RegisterProductType.php
@@ -7,11 +7,13 @@ class RegisterProductType{
     function __construct()
     {
         add_action('product_type_selector', [$this, 'add_auction_product_type'] );
+        add_action('woocommerce_product_data_tabs', [$this, 'auction_product_tab'] );
     }
 
     /**
      * Product type registered in functions.php file
-     *
+     * This will display 'Auction Product' type in Product data dropdown menu
+     * 
      * @param [type] $types
      * @return type array
      */
@@ -19,6 +21,22 @@ class RegisterProductType{
         $types[ 'fbs_auction' ] = __( 'Auction Product', 'fbs-woocommerce-auction' );
     
         return $types;    
+    }
+
+    /**
+     * This will add 'Auction Product' Tab 
+     *
+     * @param [type] $tabs
+     * @return $tabs array
+     */
+    public function auction_product_tab( $tabs) {
+            
+        $tabs['fbs_auction'] = array(
+        'label'     => __( 'Auction Product', 'fbs-woocommerce-auction' ),
+        'target' => 'fbs_auction_product_options',
+        'class'  => 'show_if_fbs_auction_product',
+        );
+        return $tabs;
     }
 
 }
