0646dde3de94deb9cbf721cd6c2d2b8f75a2d354
Fazle Bari <fazlebarisn@gmail.com>
2023-04-03 13:51:08 +0600

    set opening price

--- includes/Frontend/AuctionProduct.php
+++ includes/Frontend/AuctionProduct.php
@@ -8,16 +8,23 @@ class AuctionProduct{
 
     function __construct()
     {
-        $product_id = get_the_ID();
-        //add_filter( 'woocommerce_get_price_html', [$this, 'display_price'] );
+        add_filter( 'woocommerce_get_price_html', [$this, 'display_price'] );
     }
 
     function display_price ( $price ) {
-        //wc_price();
-        //dd($id);
-        //return $price;
-        
-
+        // Get the product id
+        $this->product_id = get_the_ID();
+        // get current product
+        $product = wc_get_product( $this->product_id );
+        // get product type
+        $product_type = $product->get_type();
+        // set opening price for auction product
+        if( 'fbsauction' == $product_type ){
+            // get price from database
+            $opening_price = get_post_meta( $this->product_id, 'fbs_opening_price', true );
+            $price = 'Opening Price ' . wc_price( $opening_price );
+        }  
+        return $price;
     }
     
 }
