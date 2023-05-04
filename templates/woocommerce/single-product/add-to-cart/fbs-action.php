f0252b1b34acc3edab0a8339716754b596734d3d
Fazle Bari <fazlebarisn@gmail.com>
2023-05-04 17:55:30 +0600

    Working

--- templates/woocommerce/single-product/add-to-cart/fbs-action.php
+++ templates/woocommerce/single-product/add-to-cart/fbs-action.php
@@ -4,11 +4,10 @@ defined( 'ABSPATH' ) || exit;
 
 global $product;
 
-
-if ( ! $product->is_purchasable() ) {
+if ( ! $product->is_purchasable() || !$product->is_sold_individually() ) {
 	return;
 }
-// dd(3333333);
+
 echo wc_get_stock_html( $product ); // WPCS: XSS ok.
 
 if ( $product->is_in_stock() ) : ?>
@@ -25,14 +24,19 @@ if ( $product->is_in_stock() ) : ?>
 			array(
 				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
 				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				// 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
 			)
 		);
 
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
+		<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" />
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		<button type="submit" class="single_add_to_cart_button button alt">
+		<?php echo apply_filters('single_add_to_cart_text',sprintf(__( 'Buy Now %s', 'ultimate-woocommerce-auction' ),wc_price($product->get_regular_price())), $product); ?></button>
+		
+		<input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" />
+        <input type="hidden" name="product_id" value="<?php echo esc_attr( $post->ID ); ?>" />
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
