506d5ff6315698eef6b4e985ab4d810a821096d9
Fazle Bari <fazlebarisn@gmail.com>
2023-05-14 11:18:28 +0600

    Solve price issue

--- includes/Admin/AddFields.php
+++ includes/Admin/AddFields.php
@@ -27,7 +27,7 @@ class AddFields{
             'name'          => 'fbs_opening_price',
             'label'         =>  __( 'Opening Price', 'fbs-woocommerce-auction' ) . ' (' . get_woocommerce_currency_symbol() . ')',
             'class'         =>  'fbs_ap_input',
-            'type'          =>  'text',
+            'type'          =>  'number',
             'desc_tip'      =>  true,
             'description'   => 'Add the opining price',
             'data_type'     => 'price'
@@ -37,20 +37,22 @@ class AddFields{
             'name'          => 'fbs_lowest_price',
             'label'         =>  __( 'Lowest Price', 'fbs-woocommerce-auction' ) . ' (' . get_woocommerce_currency_symbol() . ')',
             'class'         =>  'fbs_ap_input',
-            'type'          =>  'text',
+            'type'          =>  'number',
             'desc_tip'      =>  true,
             'description'   => 'Add the lowest price',
             'data_type'     => 'price'
         ];
         $args[] = [
-            'id'            => 'fbs_buynow_price',
-            'name'          => 'fbs_buynow_price',
-            'label'         =>  __( 'Buy Now Price', 'fbs-woocommerce-auction' ) . ' (' . get_woocommerce_currency_symbol() . ')',
-            'class'         =>  'fbs_ap_input',
-            'type'          =>  'text',
-            'desc_tip'      =>  true,
-            'description'   => 'Add buy now price',
-            'data_type'     => 'price'
+            'id'          => '_regular_price',
+            'label'       => __( 'Buy Now Price', 'woocommerce' ) . ' (' . get_woocommerce_currency_symbol() . ')',
+            'placeholder' => '',
+            'desc_tip'    => 'true',
+            'description' => __( 'Enter the buy now price for the product.', 'woocommerce' ),
+            'type'        => 'number',
+            'custom_attributes' => array(
+                'step' => 'any',
+                'min'  => '0'
+            )
         ];
         $args[] = [
             'id'            => 'fbs_bid_closing_date',
@@ -63,6 +65,8 @@ class AddFields{
             // 'description'   => 'Cloose bid closing time',
 
         ];
+
+
 
         // if need to add more field or change field data, then we can use this filter hook
         $args = apply_filters('fbs_ap_panel_field_args', $args);
@@ -96,14 +100,12 @@ class AddFields{
         // Sanitize data before input
         $fbs_opening_price = isset( $_POST['fbs_opening_price'] ) ? sanitize_text_field( $_POST['fbs_opening_price'] ) : '';
         $fbs_lowest_price = isset( $_POST['fbs_lowest_price'] ) ? sanitize_text_field( $_POST['fbs_lowest_price'] ) : '';
-        $fbs_buynow_price = isset( $_POST['fbs_buynow_price'] ) ? sanitize_text_field( $_POST['fbs_buynow_price'] ) : '';
         $fbs_bid_closing_date = isset( $_POST['fbs_bid_closing_date'] ) ? sanitize_text_field( $_POST['fbs_bid_closing_date'] ) : '';
         $fbs_product_type = isset( $_POST['fbs_product_type'] ) ? sanitize_text_field( $_POST['fbs_product_type'] ) : '';
 
         //Updating Here
         update_post_meta( $post_id, 'fbs_opening_price', esc_attr( $fbs_opening_price ) );
         update_post_meta( $post_id, 'fbs_lowest_price', esc_attr( $fbs_lowest_price ) );
-        update_post_meta( $post_id, 'fbs_buynow_price', esc_attr( $fbs_buynow_price ) );
         update_post_meta( $post_id, 'fbs_bid_closing_date', esc_attr( $fbs_bid_closing_date ) );
         update_post_meta( $post_id, 'fbs_product_type', esc_attr( $fbs_product_type ) );
     }
